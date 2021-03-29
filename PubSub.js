a0f986eb0f0bda8004481b1bb518afa93a80cbf6
coderyz <yinzhao@live.cn>
2021-03-29 09:36:41 +0800

    done PubSub-js

--- PubSub.js
+++ PubSub.js
@@ -1,5 +1,5 @@
 (function (window) {
-  const callbackContainer = {}
+  let callbackContainer = {}//制定容器
   let id = 0
 
   class PubSub {
@@ -17,10 +17,10 @@
 // 2.异步消息发布:PubSub.publish('Name',需要发布的消息)
     static publish = function (msg, data) {
       const callbacks = callbackContainer[msg]
-      if (callbacks){
-        Object.values(callbacks).forEach(callback=>{
+      if (callbacks) {
+        Object.values(callbacks).forEach(callback => {
           setTimeout(() => {
-            callback(msg,data)
+            callback(msg, data)
           })
         })
       }
@@ -28,14 +28,27 @@
 // 3.同步消息发布:PubSub.publishSync('Name',需要发布的消息)
     static publishSync = function (msg, data) {
       const callbacks = callbackContainer[msg]
-      if (callbacks){
-        Object.values(callbacks).forEach(callback=>{
-            callback(msg,data)
+      if (callbacks) {
+        Object.values(callbacks).forEach(callback => {
+          callback(msg, data)
         })
       }
     }
 // 4.取消订阅:PubSub.unsubscribe(flag),一般在beforeDestroy钩子内用
     static unsubscribe = function (flag) {
+      //  4.1:没有指定flag则取消所有消息订阅
+      if (flag === undefined) {
+        callbackContainer = {}
+      }
+      //  4.2:flag是token，取消对应的一个消息订阅
+      else if (typeof flag === 'string' && flag.indexOf('uid_') === 0) {
+        Object.values(callbackContainer).forEach(callvacks => {
+          delete callvacks[flag]
+        })
+        //  4.3:flag是msgName,取消对应的所有消息订阅
+      } else {
+        delete callbackContainer[flag]
+      }
 
     }
 
